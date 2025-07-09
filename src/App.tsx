19f47704925df8a26d2803b2e5f1430abbda1d23
Your Name <kunal.s@mistay.in>
2025-07-09 11:03:23 +0530

    feat: analytics addition

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ReactFlowProvider } from "@xyflow/react";
 import { FlowCanvas } from "./components/flow-canvas";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App() {
   return (
     <ReactFlowProvider>
       <FlowCanvas />
+      <Analytics />
     </ReactFlowProvider>
   );
 }
